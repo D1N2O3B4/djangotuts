b48d06041cfb93f831ed2b2d0b5e7f33d96b3d8b
D1N2O3B4 <davebostons2017@gmail.com>
2023-06-27 12:41:58 +0000

    added new path

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('',views.index,name='home')]
+urlpatterns = [path('',views.index,name='home'),
+               path('counter',views.count,name='counter')]
