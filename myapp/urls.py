e56d02ea7985b0d0fb3a70105fc1b56f0c85ef12
D1N2O3B4 <davebostons2017@gmail.com>
2023-06-27 21:51:32 +0000

    changed route name

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path('',views.index,name='home'),
-               path('counter',views.count,name='counter')]
+               path('counter',views.count,name='counts')]
