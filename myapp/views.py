937a380a9275dfb1e9dc60f4d02deb38b84d786e
D1N2O3B4 <davebostons2017@gmail.com>
2023-06-28 17:58:33 +0000

    created object feature

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
@@ -7,8 +8,12 @@ def index(request):
         'name':'David Boateng',
         'index_no':10888204,
         'course':'Computer Science',
-    }   
-    return render(request,'index.html',context)
+    }
+
+    feature = Feature()
+    feature.title = "About Me"
+    feature.content = "I am a student studying @ University Of Ghana.  Help is on its way please. Do you need assistance"   
+    return render(request,'index.html',{'context':context,'feature':feature})
 
 def count(request):
     text = len(request.POST['text'].split())
